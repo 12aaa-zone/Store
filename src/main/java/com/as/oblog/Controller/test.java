813638f186f1e7add37fc61fdac8e8ad23547b3f
12aaa-zone <human12aaa@gmail.com>
2023-11-08 17:27:14 +0800

    first commit

---
+++ src/main/java/com/as/oblog/Controller/test.java
@@ -0,0 +1,4 @@
+package com.as.oblog.Controller;
+
+public class test {
+}
---
+++ src/test/java/com/as/oblog/OBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.oblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
